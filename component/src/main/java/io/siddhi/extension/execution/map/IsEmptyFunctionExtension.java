de903474a2bacd47704cc366c21a036165166fa7
Niveathika <niveathika@wso2.com>
2019-09-19 18:04:24 +0530

    Add map:isEmpty() implementation

---
+++ component/src/main/java/io/siddhi/extension/execution/map/IsEmptyFunctionExtension.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c)  2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.siddhi.extension.execution.map;
+
+import io.siddhi.annotation.Example;
+import io.siddhi.annotation.Extension;
+import io.siddhi.annotation.Parameter;
+import io.siddhi.annotation.ParameterOverload;
+import io.siddhi.annotation.ReturnAttribute;
+import io.siddhi.annotation.util.DataType;
+import io.siddhi.core.config.SiddhiQueryContext;
+import io.siddhi.core.exception.SiddhiAppRuntimeException;
+import io.siddhi.core.executor.ExpressionExecutor;
+import io.siddhi.core.executor.function.FunctionExecutor;
+import io.siddhi.core.util.config.ConfigReader;
+import io.siddhi.core.util.snapshot.state.State;
+import io.siddhi.core.util.snapshot.state.StateFactory;
+import io.siddhi.query.api.definition.Attribute;
+
+import java.util.Map;
+
+/**
+ * isEmpty(Object)
+ * Returns boolean true if the object is empty map, boolean false if it is not .
+ * Accept Type(s): (Object)
+ * Return Type(s): boolean
+ */
+@Extension(
+        name = "isEmpty",
+        namespace = "map",
+        description = "Function checks if the map is empty.",
+        parameters = {
+                @Parameter(name = "map",
+                        description = "The map the need to be determined whether it's empty or not.",
+                        type = {DataType.OBJECT},
+                        dynamic = true
+                )
+        },
+        parameterOverloads = {
+                @ParameterOverload(parameterNames = {"map"})
+        },
+        returnAttributes = @ReturnAttribute(
+                description = "Returns `true` if the map is empty and `false` if otherwise.",
+                type = DataType.BOOL),
+        examples = @Example(
+                syntax = "map:isEmpty(students)",
+                description = "Returns 'true' if the students is empty else it returns `false`.")
+)
+public class IsEmptyFunctionExtension extends FunctionExecutor<State> {
+    private Attribute.Type returnType = Attribute.Type.BOOL;
+
+    @Override
+    protected StateFactory<State> init(ExpressionExecutor[] attributeExpressionExecutors,
+                                ConfigReader configReader,
+                                SiddhiQueryContext siddhiQueryContext) {
+        return null;
+    }
+
+    @Override
+    protected Object execute(Object[] data, State state) {
+        return null;
+    }
+
+    @Override
+    protected Boolean execute(Object data, State state) {
+        if (data instanceof Map) {
+            return ((Map) data).isEmpty();
+        }
+        throw new SiddhiAppRuntimeException("Dropping event since the object is not of type Map<>. " +
+                "Data: '" + data.toString() + "'.");    }
+
+    @Override
+    public Attribute.Type getReturnType() {
+        return returnType;
+    }
+}
