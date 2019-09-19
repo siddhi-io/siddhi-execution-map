53cfc65030047ded6c8a5ca47a326a422307fbff
Niveathika <niveathika@wso2.com>
2019-09-19 14:24:00 +0530

    Add implementation for map:size()

---
+++ component/src/main/java/io/siddhi/extension/execution/map/SizeFunctionExtension.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.siddhi.extension.execution.map;
+
+import io.siddhi.annotation.Example;
+import io.siddhi.annotation.Extension;
+import io.siddhi.annotation.Parameter;
+import io.siddhi.annotation.ParameterOverload;
+import io.siddhi.annotation.ReturnAttribute;
+import io.siddhi.annotation.util.DataType;
+import io.siddhi.core.config.SiddhiQueryContext;
+import io.siddhi.core.exception.SiddhiAppCreationException;
+import io.siddhi.core.exception.SiddhiAppRuntimeException;
+import io.siddhi.core.executor.ExpressionExecutor;
+import io.siddhi.core.executor.function.FunctionExecutor;
+import io.siddhi.core.util.config.ConfigReader;
+import io.siddhi.core.util.snapshot.state.State;
+import io.siddhi.core.util.snapshot.state.StateFactory;
+import io.siddhi.query.api.definition.Attribute;
+
+import java.util.Map;
+
+/**
+ * Implementation class for map:size()
+ */
+@Extension(
+        namespace = "map",
+        name = "size",
+        description = "Function to return the size of the map.",
+        parameters = {
+                @Parameter(
+                        name = "map",
+                        description = "The map for which size should be returned.",
+                        type = {DataType.OBJECT},
+                        dynamic = true
+                )
+        },
+        parameterOverloads = {
+                @ParameterOverload(parameterNames = {"map"})
+        },
+        returnAttributes = @ReturnAttribute(
+                description = "Returns size of the map (`java.util.Map`).",
+                type = DataType.INT),
+        examples = @Example(
+                syntax = "map:size(students)",
+                description = "Returns size of the `students` map.")
+)
+public class SizeFunctionExtension extends FunctionExecutor<State> {
+
+    @Override
+    protected StateFactory<State> init(ExpressionExecutor[] attributeExpressionExecutors,
+                                       ConfigReader configReader, SiddhiQueryContext siddhiQueryContext) {
+        int attributesLength = attributeExpressionExecutors.length;
+        if ((attributesLength != 1)) {
+            throw new SiddhiAppCreationException("map:size() function  should have only one parameter , " +
+                    "but found '" + attributesLength + "' parameters.");
+        }
+        if (attributeExpressionExecutors[0].getReturnType() != Attribute.Type.OBJECT) {
+            throw new SiddhiAppCreationException("The parameter 'map' in map:size() function should be of " +
+                    "type OBJECT, but found a parameter with type '" +
+                    attributeExpressionExecutors[0].getReturnType() + "'.");
+        }
+        return null;
+    }
+
+    @Override
+    public Attribute.Type getReturnType() {
+        return Attribute.Type.INT;
+    }
+
+    @Override
+    protected Object execute(Object[] data, State state) {
+        // Not reachable due to validation at init
+        return null;
+    }
+
+    @Override
+    protected Object execute(Object data, State state) {
+        if (data instanceof Map) {
+            return ((Map) data).size();
+        }
+        throw new SiddhiAppRuntimeException("Dropping event since the object is not of type Map<>. " +
+                "Data: '" + data + "'.");    }
+}
